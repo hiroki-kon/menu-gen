edf7025f18325fa00ee5de3707a608651f6b8ed1
hiroki-kon <hiroki-kon@users.noreply.github.com>
2024-06-11 15:48:36 +0900

    cors対応

--- backend/src/main/kotlin/com/example/menu_generator/MenuController.kt
+++ backend/src/main/kotlin/com/example/menu_generator/MenuController.kt
@@ -1,11 +1,9 @@
 package com.example.menu_generator
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = arrayOf("*"))
 @RequestMapping("/menus")
 class MenuController(val menuService: MenuService) {
     @GetMapping("/")
