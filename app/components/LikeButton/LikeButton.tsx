99fb033e8e7614c5a24966a88bcddc14e7695e8f
hiroki-kon <hiroki-kon@users.noreply.github.com>
2024-06-12 09:19:21 +0900

    default valueを受け取れるように

--- app/components/LikeButton/LikeButton.tsx
+++ app/components/LikeButton/LikeButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { Animated, View } from "react-native";
 
@@ -6,15 +6,24 @@ const AnimatedIcon = Animated.createAnimatedComponent(FontAwesome);
 
 interface Props {
   onPress: (state: boolean) => void;
+  defaultValue?: boolean;
 }
 
-export function LikeButton({ onPress }: Props) {
+export function LikeButton({ onPress, defaultValue = false }: Props) {
   const scale = useRef(new Animated.Value(1)).current;
   const opacity = useRef(new Animated.Value(1)).current;
   const reverseOpacity = useRef(new Animated.Value(0)).current;
-  const [liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(defaultValue);
+  const [isFirst, setIsFirst] = useState<boolean>(true);
 
   const like = (value: any) => {
+    if (isFirst === true && value === true) {
+      setIsFirst(false);
+      opacity.setValue(0);
+      reverseOpacity.setValue(1);
+      setLiked(value);
+      return;
+    }
     Animated.sequence([
       Animated.timing(scale, {
         toValue: 0.9,
@@ -52,35 +61,41 @@ export function LikeButton({ onPress }: Props) {
     setLiked(value);
   };
 
+  useEffect(() => {
+    like(liked);
+  }, []);
+
   return (
     <View>
-      <AnimatedIcon
-        name={"star"}
-        size={30}
-        style={{
-          position: "absolute",
-          opacity: reverseOpacity,
-          transform: [{ scale }],
-        }}
-        color="#F7CF31"
-        onPress={() => {
-          like(!liked);
-          onPress(!liked);
-        }}
-      />
-      <AnimatedIcon
-        name={"star-o"}
-        size={30}
-        style={{
-          opacity: opacity,
-          transform: [{ scale }],
-        }}
-        color="black"
-        onPress={() => {
-          like(!liked);
-          onPress(!liked);
-        }}
-      />
+      <>
+        <AnimatedIcon
+          name={"star"}
+          size={30}
+          style={{
+            position: "absolute",
+            opacity: reverseOpacity,
+            transform: [{ scale }],
+          }}
+          color="#F7CF31"
+          onPress={() => {
+            like(!liked);
+            onPress(!liked);
+          }}
+        />
+        <AnimatedIcon
+          name={"star-o"}
+          size={30}
+          style={{
+            opacity: opacity,
+            transform: [{ scale }],
+          }}
+          color="black"
+          onPress={() => {
+            like(!liked);
+            onPress(!liked);
+          }}
+        />
+      </>
     </View>
   );
 }
