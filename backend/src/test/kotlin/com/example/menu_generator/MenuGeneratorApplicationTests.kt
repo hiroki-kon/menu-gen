24f279395597adcbd2b3a59d211a9aa86244bc80
hiroki-kon <hiroki-kon@users.noreply.github.com>
2024-06-13 09:29:52 +0900

    weeklyへのテスト完了

--- backend/src/test/kotlin/com/example/menu_generator/MenuGeneratorApplicationTests.kt
+++ backend/src/test/kotlin/com/example/menu_generator/MenuGeneratorApplicationTests.kt
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import java.time.LocalDate
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MenuGeneratorApplicationTests(
@@ -20,13 +22,6 @@ class MenuGeneratorApplicationTests(
 	}
 
 	@Test
-	fun `初期テスト`() {
-		val response = restTemplate.getForEntity("http://localhost:$port/menus", String::class.java)
-		// レスポンスのステータスコードは OK である。
-		assertThat(response.statusCode, equalTo(HttpStatus.OK))
-	}
-
-	@Test
 	fun `recomendへのGETリクエストはOKステータスを返す`() {
 		val response = restTemplate.getForEntity("http://localhost:$port/menus/recommend?query=test", String::class.java)
 		// レスポンスのステータスコードは OK である。
@@ -34,15 +29,46 @@ class MenuGeneratorApplicationTests(
 	}
 
 //	TODO: このテストをしっかり行う
+//	@Test
+//	fun `recomendへのGETリクエストは文字を返す`() {
+//		val response = restTemplate.getForEntity("http://localhost:$port/menus/recommend?query=test", String::class.java)
+//		val body = response.body!!
+//		// レスポンスのステータスコードは OK である。
+//		assertThat(body, equalTo("hogehogehoge"))
+//	}
+
 	@Test
-	fun `recomendへのGETリクエストは文字を返す`() {
-		val response = restTemplate.getForEntity("http://localhost:$port/menus/recommend?query=test", String::class.java)
-		val body = response.body!!
-		// レスポンスのステータスコードは OK である。
-		assertThat(body, equalTo("hogehogehoge"))
+	fun `weeklyへのPOSTリクエストはOKステータスを返す`() {
+		val request = WeeklyRecipesRequest(
+			startAt = "2020-02-15",
+			recipes = arrayOf(Recipe(recipeName = "hoge", description = "fuga", ingredients = arrayOf("piyo")))
+		)
+		val response = restTemplate.postForEntity("http://localhost:$port/menus/weekly", request, Unit::class.java)
+		assertThat(response.statusCode, equalTo(HttpStatus.OK))
 	}
 
 
+	@Test
+	fun `weeklyへのGETリクエストはOKステータスを返す`() {
+		val response = restTemplate.getForEntity("http://localhost:$port/menus/weekly", WeeklyRecipesResponse::class.java)
+		assertThat(response.statusCode, equalTo(HttpStatus.OK))
+	}
 
+	@Test
+	fun `weeklyへのGETリクエストはWeeklyRecipesを返す`() {
+		val request = WeeklyRecipesRequest(
+			startAt = "2020-02-15",
+			recipes = arrayOf(Recipe(recipeName = "hoge", description = "fuga", ingredients = arrayOf("piyo")))
+		)
+		restTemplate.postForEntity("http://localhost:$port/menus/weekly", request, Unit::class.java)
+
+		val response = restTemplate.getForEntity("http://localhost:$port/menus/weekly", WeeklyRecipesResponse::class.java)
+		val weeklyRecipe = response.body!!
+		assertThat(response.statusCode, equalTo(HttpStatus.OK))
+		assertThat(weeklyRecipe.startAt, equalTo(LocalDate.parse("2020-02-15")))
+		assertThat(weeklyRecipe.recipes[0].recipeName, equalTo("hoge"))
+		assertThat(weeklyRecipe.recipes[0].description, equalTo("fuga"))
+		assertThat(weeklyRecipe.recipes[0].ingredients, equalTo(arrayOf("piyo")))
+	}
 
 }
